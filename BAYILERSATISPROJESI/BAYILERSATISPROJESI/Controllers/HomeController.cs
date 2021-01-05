3af021d45442028d5733c2c811f5ef255a242d16
aysenurperker15 <74301727+aysenurperker15@users.noreply.github.com>
2021-01-05 22:17:45 +0300

    NEWEDIT

---
+++ BAYILERSATISPROJESI/BAYILERSATISPROJESI/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BAYILERSATISPROJESI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Urunler()
+        {
+            return View();
+        }
+        public ActionResult YoneticiGirisi()
+        {
+            return View();
+        }
+        public ActionResult BayiGirisi()
+        {
+            return View();
+        }
+        public ActionResult YOneticiSinirlar()
+        {
+            return View();
+        }
+    }
+}
---
+++ BAYILERSATISPROJESI/BAYILERSATISPROJESI/Controllers/KategoriController.cs
@@ -0,0 +1,29 @@
+﻿using BAYILERSATISPROJESI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BAYILERSATISPROJESI.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [ChildActionOnly]
+        public PartialViewResult KategoriMenu()
+        {
+            List<Kategori> kategoriler = new List<Kategori>()
+            {
+                new Kategori() { Id = 1, KategoriAdi = "Kamyonet Liftleri"},
+                new Kategori() { Id = 2, KategoriAdi = "Kamyon Liftleri"},
+                new Kategori() { Id = 3, KategoriAdi = "Tır Liftleri"}
+            };
+            return PartialView("KategoriMenu", kategoriler);
+        }
+    }
+}
