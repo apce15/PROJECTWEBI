bb4494f623f37928138d0ba33fea1cdf86abdb68
aysenurperker15 <nurperker15@gmail.com>
2021-01-06 05:39:00 +0300

    noluredit

--- BAYILERSATISPROJESI/BAYILERSATISPROJESI/Controllers/HomeController.cs
+++ BAYILERSATISPROJESI/BAYILERSATISPROJESI/Controllers/HomeController.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using BayiSatisProjesi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
-namespace BAYILERSATISPROJESI.Controllers
+namespace BayiSatisProjesi.Controllers
 {
     public class HomeController : Controller
     {
@@ -13,7 +13,42 @@ namespace BAYILERSATISPROJESI.Controllers
         {
             return View();
         }
+        public ActionResult Anasayfa()
+        {
+            return View();
+        }
         public ActionResult Urunler()
+        {
+
+
+
+
+            UrunKategoriModel model = new UrunKategoriModel();
+
+
+            model.UrunSayisi = Veritabani.Liste.Where(i => i.Satistami == true).Count();
+            model.Urunler = Veritabani.Liste.Where(i => i.Satistami == true).ToList();
+
+
+
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var urun = Veritabani.Liste.Where(i => i.UrunId == id).FirstOrDefault();
+
+
+
+            return View(urun);
+        }
+
+        private object Where(Func<object, bool> p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult BayiGirisi()
         {
             return View();
         }
@@ -21,13 +56,7 @@ namespace BAYILERSATISPROJESI.Controllers
         {
             return View();
         }
-        public ActionResult BayiGirisi()
-        {
-            return View();
-        }
-        public ActionResult YOneticiSinirlar()
-        {
-            return View();
-        }
-    }
+        
+
+}
 }
