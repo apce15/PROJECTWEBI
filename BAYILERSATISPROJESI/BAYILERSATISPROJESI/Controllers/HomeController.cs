b9d4ba9f14c7b4621bc2a60b3672b854d7a45b6a
esrce <esra.aka@bil.omu.edu.tr>
2021-01-09 23:39:17 +0300

    esraedityeni

--- BAYILERSATISPROJESI/BAYILERSATISPROJESI/Controllers/HomeController.cs
+++ BAYILERSATISPROJESI/BAYILERSATISPROJESI/Controllers/HomeController.cs
@@ -1,10 +1,10 @@
-﻿using BayiSatisProjesi.Models;
+﻿using BAYILERSATISPROJESI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
-namespace BayiSatisProjesi.Controllers
+namespace BAYILERSATISPROJESI.Controllers
 {
     public class HomeController : Controller
     {
